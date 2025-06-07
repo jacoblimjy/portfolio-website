868ba5dbf49891b0666776410b5c1e133e0dc598
Lim Jacob <111573383+jacoblimjy@users.noreply.github.com>
2025-06-07 18:01:02 +0800

    Ensure GitHub and LinkedIn icons match

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -4,9 +4,8 @@ import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { BsArrowRight, BsLinkedin, BsGithub } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
-import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
 import { useState, useEffect } from "react";
@@ -116,10 +115,10 @@ export default function Intro() {
                                                 target="_blank"
                                                 rel="noopener noreferrer"
                                         >
-                                                <FaGithubSquare className="w-6 h-6" />
+                                                <BsGithub className="w-6 h-6" />
                                         </a>
                                 </div>
-			</motion.div>
+                        </motion.div>
 
 			{/* <motion.div
 				className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
@@ -159,12 +158,12 @@ export default function Intro() {
 
 				<a
 					className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-					href="https://github.com/jacoblimjy"
-					target="_blank"
-				>
-					<FaGithubSquare />
-				</a>
-			</motion.div> */}
-		</section>
-	);
+                                        href="https://github.com/jacoblimjy"
+                                        target="_blank"
+                                >
+                                        <BsGithub />
+                                </a>
+                        </motion.div> */}
+                </section>
+        );
 }
